c6d81b76384ec767b13615a21cfe7995afc47e67
Gnourt812005 <txt812005@gmail.com>
2025-08-29 23:25:09 +0700

    add page image-storage, add delete image

---
+++ src/app/admin/image-storage/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import ImageGallery from "@/features/image-storage/components/image-gallery"
+
+export default function AdminImageStoragePage() {
+  return (
+    <div>
+      <ImageGallery/>
+    </div>
+  )
+
+}
+
+
