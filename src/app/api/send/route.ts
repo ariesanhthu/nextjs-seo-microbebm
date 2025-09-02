4854cc0b40836b7611670d0bcfeb4a324d41f403
unknown <anhthuxitin@outlook.com>
2025-09-02 20:50:08 +0700

    hotfix: remove console email

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -4,8 +4,8 @@ export async function POST(req: Request)
 {
     try {
         const { recipient, subject, body } = await Promise.resolve(req.json());
-        console.log("=== API /api/send POST called ===");
-        console.log("Request body:", { recipient, subject, body });
+        // console.log("=== API /api/send POST called ===");
+        // console.log("Request body:", { recipient, subject, body });
 
         // Lấy API key từ .env.local
         const apiKey = process.env.SHARED_SECRET;
@@ -25,7 +25,7 @@ export async function POST(req: Request)
         });
 
         const data = await res.json();
-        console.log("Response data:", data);
+        // console.log("Response data:", data);
         return NextResponse.json(data);
     }
     catch (error: any) {
